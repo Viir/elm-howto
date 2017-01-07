556a7b1e83a29e18f5581f690e919da8b5e1d9fc
Michael Rätzel <Viir@Viir.de>
2017-01-07 14:02:48 +0100

    Start a new playback each time the button is clicked without interfering with playbacks started previously.

--- src/audio-start-playback.elm
+++ src/audio-start-playback.elm
@@ -1,17 +1,26 @@
 import Html exposing (beginnerProgram, div, button, text, audio, source)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 
 audioFileUrl = "http://dict.leo.org/media/audio/ZXOapx_FyRojukaMRHKS_w.ogg"
 
+type alias Model = List String
+
 main =
-  beginnerProgram { model = 0, view = view, update = update }
+  beginnerProgram { model = [], view = view, update = update }
+
+type Msg = StartPlayback
 
 playbackView audioFileUrl =
-  audio [ controls True, autoplay True ]
+  audio [ controls False, autoplay True ]
     [ source [ src audioFileUrl ] [] ]
 
 view model =
   div []
-    [ playbackView audioFileUrl ]
+    [ div []
+      [ button [ onClick StartPlayback ] [ text "click to play audio" ]],
+      div [] (model |> List.map playbackView) ]
 
-update msg model = model
+update msg model =
+  case msg of
+    StartPlayback -> model |> List.append [audioFileUrl]
