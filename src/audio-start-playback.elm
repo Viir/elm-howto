4a493dc9883c167cfc3360e023c54fe4ea844c6f
Michael Rätzel <Viir@Viir.de>
2017-01-07 14:49:27 +0100

    Draw users attention to dependency on audio file url and enable configuration of URL.

--- src/audio-start-playback.elm
+++ src/audio-start-playback.elm
@@ -1,26 +1,47 @@
-import Html exposing (beginnerProgram, div, button, text, audio, source)
+import Html exposing (beginnerProgram, div, button, text, audio, source, input, table)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onClick)
+import Html.Events exposing (onClick, onInput)
 
-audioFileUrl = "http://dict.leo.org/media/audio/ZXOapx_FyRojukaMRHKS_w.ogg"
+type alias Model = {
+  selectedAudioSourceUrl : String,
+  listPlayback : List String
+}
 
-type alias Model = List String
+init : Model
+init =
+  { selectedAudioSourceUrl = "http://dict.leo.org/media/audio/ZXOapx_FyRojukaMRHKS_w.ogg",
+    listPlayback = []
+  }
 
 main =
-  beginnerProgram { model = [], view = view, update = update }
+  beginnerProgram { model = init, view = view, update = update }
 
-type Msg = StartPlayback
+type Msg =
+  NewSourceUrl String |
+  StartPlayback
 
 playbackView audioFileUrl =
   audio [ controls False, autoplay True ]
     [ source [ src audioFileUrl ] [] ]
 
 view model =
-  div []
-    [ div []
-      [ button [ onClick StartPlayback ] [ text "click to play audio" ]],
-      div [] (model |> List.map playbackView) ]
+  div [] [
+      div [] [
+        text "configure audio parameters",
+        div [ style [("margin", "10px")]] [
+          text "url to audio file",
+          div [] [input [ placeholder "url to audio file", onInput NewSourceUrl, value model.selectedAudioSourceUrl ] []]
+        ],
+        button [ onClick StartPlayback ] [ text "click to start playback" ]],
+      div [] (model.listPlayback |> List.map playbackView) ]
 
+startPlayback model =
+  { model | listPlayback = [ model.listPlayback, [model.selectedAudioSourceUrl]] |> List.concat }
+
+update : Msg -> Model -> Model
 update msg model =
   case msg of
-    StartPlayback -> model |> List.append [audioFileUrl]
+    NewSourceUrl url ->
+      { model | selectedAudioSourceUrl = url}
+    StartPlayback ->
+      (startPlayback model)
